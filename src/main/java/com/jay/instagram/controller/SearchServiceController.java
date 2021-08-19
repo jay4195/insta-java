c1457c4ab35acf89114baa03ba4e11f39bcd5fad
ZHIYUAN WANG(IT) <jay4195@qq.com>
2021-08-19 14:47:10 +0800

    kafka group id

--- src/main/java/com/jay/instagram/controller/SearchServiceController.java
+++ src/main/java/com/jay/instagram/controller/SearchServiceController.java
@@ -1,5 +1,7 @@
 package com.jay.instagram.controller;
 
+import com.jay.instagram.bean.Post;
+import com.jay.instagram.service.PostService;
 import com.jay.instagram.service.SearchEngineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @RestController
@@ -16,9 +19,23 @@ public class SearchServiceController {
     @Autowired
     SearchEngineService searchEngineService;
 
+    @Autowired
+    PostService postService;
+
     @RequestMapping(value = "/{query}",
             method = RequestMethod.GET)
-    List<Long> search(@PathVariable("query") String query) {
+    List<Post> search(@PathVariable("query") String query) {
+        List<Post> postResults = new LinkedList<>();
+        List<Long> postIds = searchEngineService.search(query);
+        for (Long id : postIds) {
+            postResults.add(postService.getPost(id));
+        }
+        return postResults;
+    }
+
+    @RequestMapping(value = "/index/{query}",
+            method = RequestMethod.GET)
+    List<Long> searchIndexs(@PathVariable("query") String query) {
         return searchEngineService.search(query);
     }
 }
--- src/main/java/com/jay/instagram/kafka/KafkaConsumer.java
+++ src/main/java/com/jay/instagram/kafka/KafkaConsumer.java
@@ -26,10 +26,11 @@ public class KafkaConsumer {
         //System.out.println("简单消费："+record.topic()+"-"+record.partition()+"-"+record.value());
         Post post = JSON.parseObject((String) record.value(), Post.class);
         postService.createPost(post);
+        log.info("[Kafka Listener] Create Post id {}", post.getId());
         kafkaTemplate.send("post-after", JSON.toJSON(post).toString());
     }
 
-    @KafkaListener(topics = {"delete-post"})
+    @KafkaListener(topics = {"delete-post"}, id = "base")
     public void onDeletePost(ConsumerRecord<?, ?> record){
         String value = (String) record.value();
         Long id = Long.parseLong(value);
